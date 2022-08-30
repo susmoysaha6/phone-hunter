f5dc07d2e0461450dcdc8ab3ccd7992927a7d5fc
susmoysaha6 <susmoysaha6@gmail.com>
2022-08-30 17:15:25 +0600

    Show details button functionality added

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ const displayPhone = (phones, dataLimit) => {
                     <h5 class="card-title">${phone.phone_name}</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural
                                 lead-in to additional content. This content is a little bit longer.</p>
+                    <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Show Detail</button>
                 </div>
             </div>
         `;
@@ -53,9 +54,15 @@ const processSearch = (dataLimit) => {
     loadPhone(searchText, dataLimit);
     // searchField.value = '';
 }
-
+// handle search button click
 document.getElementById('btn-search').addEventListener('click', function () {
     processSearch(10);
+})
+// search input field enter key handler
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        processSearch(10);
+    }
 })
 
 const toggleSpinner = isLoading => {
@@ -71,4 +78,27 @@ const toggleSpinner = isLoading => {
 document.getElementById('btn-show-all').addEventListener('click', function () {
     processSearch();
 })
+
+const loadPhoneDetails = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetail(data.data);
+}
+
+const displayPhoneDetail = (phone) => {
+    console.log(phone);
+    const modalTile = document.getElementById('phoneDetailModalLabel');
+    modalTile.innerText = phone.name;
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = `
+        <div class="text-center">
+            <p>${phone.releaseDate ? phone.releaseDate : "No Release Date Found"}</p>
+            <img src="${phone.image}" alt="" />
+            <p>Memory: ${phone.mainFeatures.memory}</p>
+            <p>Display: ${phone.mainFeatures.displaySize}</p>
+            <p>Chip-Set: ${phone.mainFeatures.chipSet}</p>
+        </div>
+    `;
+}
 // loadPhone()
